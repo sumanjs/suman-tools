be188766ff0bd0315032b680ed85a58406d2a7cf
Olegzandr VD <alex@oresoftware.com>
2017-07-05 19:43:09 -0700

    pdev:set

--- index.js
+++ index.js
@@ -1,103 +1,89 @@
 #!/usr/bin/env node
 'use strict';
-
-//core
-const fs = require('fs');
-const util = require('util');
-const path = require('path');
-
-//npm
-const dashdash = require('dashdash');
-const {findProjectRoot} = require('residence');
-const cp = require('child_process');
-const chalk = require('chalk');
-
-//project
-
-let options = [
-  {
-    names: ['help', 'h'],
-    type: 'bool',
-    help: 'Print this help and exit.'
-  },
-  {
-    names: ['extract-json-array'],
-    type: 'string',
-    help: 'Use this option to send each item of a JSON array to stdout.'
-  },
-  {
-    names: ['is-symlinked-globally'],
-    type: 'string',
-    help: 'Use this option to send each item of a JSON array to stdout.'
-  },
-  {
-    names: ['is-symlinked-locally'],
-    type: 'string',
-    help: 'Use this option to send each item of a JSON array to stdout.'
-  },
+Object.defineProperty(exports, "__esModule", { value: true });
+var fs = require("fs");
+var path = require("path");
+var cp = require("child_process");
+var dashdash = require('dashdash');
+var findProjectRoot = require('residence').findProjectRoot;
+var chalk = require("chalk");
+var options = [
+    {
+        names: ['help', 'h'],
+        type: 'bool',
+        help: 'Print this help and exit.'
+    },
+    {
+        names: ['extract-json-array'],
+        type: 'string',
+        help: 'Use this option to send each item of a JSON array to stdout.'
+    },
+    {
+        names: ['is-symlinked-globally'],
+        type: 'string',
+        help: 'Use this option to send each item of a JSON array to stdout.'
+    },
+    {
+        names: ['is-symlinked-locally'],
+        type: 'string',
+        help: 'Use this option to send each item of a JSON array to stdout.'
+    },
 ];
-
-let opts, parser = dashdash.createParser({options: options});
-
+var opts, parser = dashdash.createParser({ options: options });
 try {
-  opts = parser.parse(process.argv);
-} catch (e) {
-  console.error(chalk.red(' => suman-tools usage error: %s'), e.message);
-  process.exit(1);
+    opts = parser.parse(process.argv);
 }
-
+catch (e) {
+    console.error(chalk.red(' => suman-tools usage error: %s'), e.message);
+    process.exit(1);
+}
 if (opts.help) {
-  let help = parser.help({includeEnv: true}).trimRight();
-  console.log('usage: node foo.js [OPTIONS]\n'
-    + 'options:\n'
-    + help);
-  process.exit(0);
+    var help = parser.help({ includeEnv: true }).trimRight();
+    console.log('usage: node foo.js [OPTIONS]\n'
+        + 'options:\n'
+        + help);
+    process.exit(0);
 }
-
 if (opts.extract_json_array) {
-  const arr = JSON.parse(opts.extract_json_array);
-  arr.forEach(function (a) {
-    console.log(a);
-  });
-  process.exit(0);
+    var arr = JSON.parse(opts.extract_json_array);
+    arr.forEach(function (a) {
+        console.log(a);
+    });
+    process.exit(0);
 }
-
-const root = findProjectRoot(process.cwd());
-
+var root = findProjectRoot(process.cwd());
 if (opts.is_symlinked_locally) {
-  try {
-    const p = path.resolve(root + '/node_modules/' + String(opts.is_symlinked_locally).trim());
-    const stats = fs.lstatSync(p);
-    let v = stats.isSymbolicLink() ? 'affirmative' : 'negative';
-    console.log(v);
-    process.exit(0);
-  }
-  catch (err) {
-    if(/no such file or directory/.exec(err.message)){
-      console.log('negative');
-      process.exit(0);
+    try {
+        var p = path.resolve(root + '/node_modules/' + String(opts.is_symlinked_locally).trim());
+        var stats = fs.lstatSync(p);
+        var v = stats.isSymbolicLink() ? 'affirmative' : 'negative';
+        console.log(v);
+        process.exit(0);
     }
-    console.error('\n', err.stack, '\n');
-    process.exit(1);
-  }
-
+    catch (err) {
+        if (/no such file or directory/.exec(err.message)) {
+            console.log('negative');
+            process.exit(0);
+        }
+        console.error('\n', err.stack, '\n');
+        process.exit(1);
+    }
 }
-
 if (opts.is_symlinked_globally) {
-  try {
-    const npmRoot = String(cp.execSync('npm root -g', {encoding: 'utf8'})).trim();
-    const p = path.resolve(npmRoot + '/' + String(opts.is_symlinked_globally).trim());
-    const stats = fs.lstatSync(p);
-    let v = stats.isSymbolicLink() ? 'affirmative' : 'negative';
-    console.log(v);
-    process.exit(0);
-  }
-  catch (err) {
-    if(/no such file or directory/.exec(err.message)){
-      console.log('negative');
-      process.exit(0);
+    try {
+        var npmRoot = String(cp.execSync('npm root -g', { encoding: 'utf8' })).trim();
+        var p = path.resolve(npmRoot + '/' + String(opts.is_symlinked_globally).trim());
+        var stats = fs.lstatSync(p);
+        var v = stats.isSymbolicLink() ? 'affirmative' : 'negative';
+        console.log(v);
+        process.exit(0);
     }
-    console.error('\n', err.stack, '\n');
-    process.exit(1);
-  }
+    catch (err) {
+        if (/no such file or directory/.exec(err.message)) {
+            console.log('negative');
+            process.exit(0);
+        }
+        console.error('\n', err.stack, '\n');
+        process.exit(1);
+    }
 }
---
+++ index.ts
@@ -0,0 +1,105 @@
+#!/usr/bin/env node
+'use strict';
+
+//core
+import * as fs from 'fs';
+import * as path from 'path';
+import * as util from 'util';
+import * as assert from 'assert';
+import * as cp from 'child_process';
+
+
+//npm
+const dashdash = require('dashdash');
+const {findProjectRoot} = require('residence');
+import * as chalk from 'chalk';
+
+//project
+
+let options = [
+  {
+    names: ['help', 'h'],
+    type: 'bool',
+    help: 'Print this help and exit.'
+  },
+  {
+    names: ['extract-json-array'],
+    type: 'string',
+    help: 'Use this option to send each item of a JSON array to stdout.'
+  },
+  {
+    names: ['is-symlinked-globally'],
+    type: 'string',
+    help: 'Use this option to send each item of a JSON array to stdout.'
+  },
+  {
+    names: ['is-symlinked-locally'],
+    type: 'string',
+    help: 'Use this option to send each item of a JSON array to stdout.'
+  },
+];
+
+let opts, parser = dashdash.createParser({options: options});
+
+try {
+  opts = parser.parse(process.argv);
+} catch (e) {
+  console.error(chalk.red(' => suman-tools usage error: %s'), e.message);
+  process.exit(1);
+}
+
+if (opts.help) {
+  let help = parser.help({includeEnv: true}).trimRight();
+  console.log('usage: node foo.js [OPTIONS]\n'
+    + 'options:\n'
+    + help);
+  process.exit(0);
+}
+
+if (opts.extract_json_array) {
+  const arr = JSON.parse(opts.extract_json_array);
+  arr.forEach(function (a: string) {
+    console.log(a);
+  });
+  process.exit(0);
+}
+
+const root = findProjectRoot(process.cwd());
+
+if (opts.is_symlinked_locally) {
+  try {
+    const p = path.resolve(root + '/node_modules/' + String(opts.is_symlinked_locally).trim());
+    const stats = fs.lstatSync(p);
+    let v = stats.isSymbolicLink() ? 'affirmative' : 'negative';
+    console.log(v);
+    process.exit(0);
+  }
+  catch (err) {
+    if(/no such file or directory/.exec(err.message)){
+      console.log('negative');
+      process.exit(0);
+    }
+    console.error('\n', err.stack, '\n');
+    process.exit(1);
+  }
+
+}
+
+if (opts.is_symlinked_globally) {
+  try {
+    const npmRoot = String(cp.execSync('npm root -g', {encoding: 'utf8'})).trim();
+    const p = path.resolve(npmRoot + '/' + String(opts.is_symlinked_globally).trim());
+    const stats = fs.lstatSync(p);
+    let v = stats.isSymbolicLink() ? 'affirmative' : 'negative';
+    console.log(v);
+    process.exit(0);
+  }
+  catch (err) {
+    if(/no such file or directory/.exec(err.message)){
+      console.log('negative');
+      process.exit(0);
+    }
+    console.error('\n', err.stack, '\n');
+    process.exit(1);
+  }
+}
